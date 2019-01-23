b46947dbc834bd72431a7aa5c9020e98eccbaae5
Ben Flanders <benflanders@Bens-MacBook-Pro.local>
2019-01-22 23:14:27 -0500

    update_progress method, takes in a single string value corresponding to the material mined

--- src/goal_handler.py
+++ src/goal_handler.py
@@ -6,10 +6,7 @@ class goal:
         self.completion_percent = 0 #max 100
         temp_file_output = "copper-ore,5;tin-ore,5"
 
-        parsed_goals = self.parse_goal_input(temp_file_output)
-        for goal in parsed_goals:
-            print(goal)
-            print(parsed_goals[goal])
+        self.curr_goals = self.parse_goal_input(temp_file_output) #parse the provided goals into a dictionary
 
 
     #convert a json file to a dictionary of goals
@@ -18,14 +15,39 @@ class goal:
         split_goals = raw_goal.split(';') #goals are delimited by semicolons
         for goal in split_goals:
             single_goal = goal.split(',') #split again at each comma
-            parsed_goals[single_goal[0]] = single_goal[1] #first part is the target, second is the amount
+            parsed_goals[single_goal[0]] = int(single_goal[1]) #first part is the target, second is the amount
         return parsed_goals
 
 
     def update_progress(self, update_string):
-        #TODO
-        #if a string is matched 
+        #prepare the input string
+        update_string = update_string.strip()
+        update_string = update_string.replace(" ", "")
+
+        try:
+            x = self.curr_goals[update_string]
+            x = x-1
+            self.curr_goals[update_string] = x
+            self.print_current_goals()
+        except:
+            print(update_string, " is not an accepted value")
+
+
+        #x = x-1
+        #self.curr_goals[update_string] = x
+        
+        #if a string is matched then decrement amount of that goal required
         return 0
         #return the current progress completion
+    
+    #print out the goals and their values
+    def print_current_goals(self):
+
+        for goal in self.curr_goals:
+            print(goal)
+            print(self.curr_goals[goal])
+
 
 gl = goal()
+gl.print_current_goals()
+print(gl.update_progress('tin-ore'))
